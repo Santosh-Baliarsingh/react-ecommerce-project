429b5e4bb74bb3d4ccc8f1c0fbc939ae62eee26e
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-04 19:26:53 +0530

    Trusted.js File Updated

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Trusted() {
   return (
-    <div>Trusted</div>
-  )
+    <>
+      <div className="container my-3">
+        <h5 className="my-2 text-center">Trusted By 10000+ Companies</h5>
+        <div className="row p-2">
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand1.png" alt="brand1" />
+          </div>
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand3.png" alt="brand2" />
+          </div>
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand4.png" alt="brand3" />
+          </div>
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand5.png" alt="brand4" />
+          </div>
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand6.png" alt="brand5" />
+          </div>
+          <div className="col-md-2 d-flex justify-content-center">
+            <img className="img-fluid" src="/assets/tbrand7.png" alt="brand5" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
