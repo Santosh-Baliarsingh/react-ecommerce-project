ad08f4701e68849b3d468971aaaa8a3b93b05a6d
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-12 18:00:38 +0530

    ErrorPage.js Added

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function ErrorPage() {
   return (
     <>
-      <h2>Error Page</h2>
+      <div className="container px-5">
+        <img
+          className="img-fluid"
+          src="/assets/page_not_found.jpg"
+          alt="error img"
+        />
+      </div>
     </>
   );
 }
