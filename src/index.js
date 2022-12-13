57c90320e2ca3fbfe4dac9cbdf534ef5d737aad9
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-13 20:09:37 +0530

    Index.js FIle Updated With AppProvider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import { AppProvider } from "./context/ProductContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AppProvider>
     <App />
-  </React.StrictMode>
+  </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
