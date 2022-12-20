03836bbc6cfdaed097b1e77216c553e74f54f260
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-20 22:42:50 +0530

    MyImage.js Component Added

---
+++ src/components/MyImage.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function MyImage({ images = [{ url: "" }] }) {
+  const [mainImage, setMainImage] = useState(images[0]);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            {images.map((currentElement, id) => (
+              <div className="row">
+                <img
+                  className="img-fluid my-2 px-0  rounded-2"
+                  key={id}
+                  src={currentElement.url}
+                  alt={currentElement.filename}
+                  onClick={() => setMainImage(currentElement)}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="col-md-8 my-auto">
+            <img
+              className="img-fluid rounded-2"
+              src={mainImage.url}
+              alt={mainImage.filename}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
