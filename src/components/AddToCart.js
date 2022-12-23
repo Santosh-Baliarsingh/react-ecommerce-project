fd99f39d96745133b3b2bea891e6fd3fe6019244
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-23 23:27:50 +0530

    AddToCart.js Component Added

---
+++ src/components/AddToCart.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { GiCheckMark } from "react-icons/gi";
+import CartItemToggle from "./CartItemToggle";
+import { NavLink } from "react-router-dom";
+
+export default function AddToCart({ product }) {
+  const { colors, stock } = product;
+  const [color, setColor] = useState(colors[0]);
+
+  const [amount, setAmount] = useState(1);
+
+  const setIncrement = () => {
+    amount < stock ? setAmount(amount + 1) : setAmount(stock);
+  };
+
+  const setDecrement = () => {
+    amount > 1 ? setAmount(amount - 1) : setAmount(1);
+  };
+
+  return (
+    <>
+      <div className="container my-3 px-0">
+        <div>
+          <p>
+            Colors :
+            {colors.map((currentColor, index) => (
+              <button
+                key={index}
+                className={
+                  color === currentColor
+                    ? "btn rounded-pill mx-2 active border-0 "
+                    : "btn rounded-pill mx-2 p-3"
+                }
+                onClick={() => setColor(currentColor)}
+                style={{ backgroundColor: currentColor }}
+              >
+                {color === currentColor ? (
+                  <GiCheckMark className="text-light" />
+                ) : null}
+              </button>
+            ))}
+          </p>
+        </div>
+        {/* Add to cart */}
+        <CartItemToggle
+          amount={amount}
+          setIncrement={setIncrement}
+          setDecrement={setDecrement}
+        />
+      </div>
+      <NavLink to="/cart">
+        <button className="btn btn-outline-warning fw-bold p-2 mx-auto">Add to Cart</button>
+      </NavLink>
+    </>
+  );
+}
