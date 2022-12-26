13410cf9ea56b482f95e8de89b28961280224da9
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-26 22:46:17 +0530

    FilterReducer.js added

---
+++ src/reducer/FilterReducer.js
@@ -0,0 +1,20 @@
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: [...action.payload],
+        all_products: [...action.payload],
+      };
+
+      case "SET_GRIDVIEW":
+        return{
+            ...state,
+            grid_view : true,
+        }   
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
