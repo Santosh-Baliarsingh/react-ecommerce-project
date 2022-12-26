12362d714b5fa966326e92d699476a7f6ca715dd
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-12-26 22:44:36 +0530

    Components Added in Products.js file

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,26 @@
 import React from "react";
+import FilterSection from "./components/FilterSection";
+import Sort from "./components/Sort";
+import ProductList from "./components/ProductList";
 
 export default function Products() {
   return (
     <>
-      <h2>Products</h2>
+      <div className="container ">
+        <div className="row">
+          <div className="col-3">
+            <FilterSection />
+          </div>
+          <div className="col-9">
+            <div className="row">
+              <Sort />
+            </div>
+            <div className="row">
+              <ProductList />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
