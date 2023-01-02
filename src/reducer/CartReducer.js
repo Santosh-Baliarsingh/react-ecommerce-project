2b7c97ed837486a4682c9989ff388c5c033aed0b
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2023-01-02 22:34:50 +0530

    CartReducer.js Added

---
+++ src/reducer/CartReducer.js
@@ -0,0 +1,34 @@
+export const CartReducer = (state, action) => {
+  if (action.type === "ADD_TO_CART") {
+    let { id, color, amount, product } = action.payload;
+    // console.log(product);
+
+    let cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    let updatedCart = state.cart.filter(
+      (currentElement) => currentElement.id !== action.payload
+    );
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  }
+
+  return state;
+};
+
+export default CartReducer;
