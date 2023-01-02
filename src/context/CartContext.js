3613946e52648da2a3029a9271a731f3dd828284
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2023-01-02 22:34:16 +0530

    CartContext.js Added

---
+++ src/context/CartContext.js
@@ -0,0 +1,35 @@
+import { createContext, useReducer, useContext } from "react";
+import reducer from "../reducer/CartReducer";
+
+const CartContext = createContext();
+
+const initialState = {
+  cart: [],
+  total_item: "",
+  total_amount: "",
+  shipping_fee: 5000,
+};
+
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Add to Cart Functionality
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
+  return (
+    <CartContext.Provider value={{ ...state, addToCart , removeItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCartContext = () => {
+  return useContext(CartContext);
+};
+
+export { CartProvider, useCartContext };
